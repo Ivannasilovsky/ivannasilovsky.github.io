f0450eb348607d289af1cdb0e89a0a51ff8a9eb2
Ivannasilovsky <nasilovskyivan@gmail.com>
2025-10-20 02:57:58 +0000

    c

--- Figura.cs
+++ Figura.cs
@@ -6,11 +6,21 @@ namespace Figuras
     public class Figura
     {
         protected Color colorFigura;
+        protected static Random random = new Random();
         
         public virtual void Dibujar(Pen pen, Graphics graphics, int x, int y)
         {
         }
-
+        
+        protected Color GenerarColorAleatorio()
+        {
+            int r = random.Next(0, 256);  
+            int g = random.Next(0, 256); 
+            int b = random.Next(0, 256);  
+            
+            return Color.FromArgb(r, g, b);
+        }
+        
         public void SetColor(Color color)
         {
             this.colorFigura = color;
@@ -22,17 +32,16 @@ namespace Figuras
         protected int alto;
         protected int ancho;
         
-        // Constructor
         public Rectangulo(int ancho, int alto)
         {
             this.ancho = ancho;
             this.alto = alto;
-            this.colorFigura = Color.Blue;
+
+            this.colorFigura = GenerarColorAleatorio();
         }
 
         public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
         {
-            
             pen.Color = this.colorFigura;
             
             Point[] points = new Point[4]
@@ -49,11 +58,9 @@ namespace Figuras
 
     public class Cuadrado : Rectangulo
     {
-        
         public Cuadrado(int lado) : base(lado, lado)
         {
-        
-            this.colorFigura = Color.Green;
+            this.colorFigura = GenerarColorAleatorio();
         }
     }
 
@@ -61,17 +68,14 @@ namespace Figuras
     {
         private int radio;
         
-        
         public Circulo(int radio)
         {
             this.radio = radio;
-      
-            this.colorFigura = Color.Red;
+            this.colorFigura = GenerarColorAleatorio();
         }
 
         public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
         {
-        
             pen.Color = this.colorFigura;
             
             graphics.DrawEllipse(pen, x, y, radio, radio);
