022c0fe78dfaca04a0b51ecb3d080b2118644260
Ivannasilovsky <nasilovskyivan@gmail.com>
2025-10-20 02:10:43 +0000

    a) figuras con trazo rojo

---
+++ Figura.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+
+namespace Figuras
+{
+    public class Figura
+    {
+        public virtual void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+        }
+    }
+
+    public class Rectangulo : Figura
+    {
+        protected int alto;
+        protected int ancho;
+        
+        // Constructor
+        public Rectangulo(int ancho, int alto)
+        {
+            this.ancho = ancho;
+            this.alto = alto;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            // MODIFICACIÓN a): Establecer color rojo usando la propiedad Color
+            pen.Color = Color.Red;
+            
+            Point[] points = new Point[4]
+            {
+                new Point(x, y),
+                new Point(x + ancho, y),
+                new Point(x + ancho, y + alto),
+                new Point(x, y + alto)
+            };
+            
+            graphics.DrawPolygon(pen, points);
+        }
+    }
+
+    public class Cuadrado : Rectangulo
+    {
+        // Constructor. Un cuadrado es un rectangulo con ancho = alto
+        public Cuadrado(int lado) : base(lado, lado)
+        {
+        }
+    }
+
+    public class Circulo : Figura
+    {
+        private int radio;
+        
+        // Constructor
+        public Circulo(int radio)
+        {
+            this.radio = radio;
+        }
+
+        public override void Dibujar(Pen pen, Graphics graphics, int x, int y)
+        {
+            // MODIFICACIÓN a): Establecer color rojo usando la propiedad Color
+            pen.Color = Color.Red;
+            
+            graphics.DrawEllipse(pen, x, y, radio, radio);
+        }
+    }
+}
